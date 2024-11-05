d4dd7ed6e1a8f16d6685c25e1cbf0a5ff0b534b2
patanAziz <patanabdulaziz73@gmail.com>
2024-11-05 23:44:26 +0530

    Update page.jsx

--- src/app/(root)/becomeadriver/page.jsx
+++ src/app/(root)/becomeadriver/page.jsx
@@ -7,7 +7,7 @@ const page = () => {
       <div className="bg-gray-100 p-6 rounded-lg max-w-lg mx-auto shadow-md">
         <h3 className="text-2xl font-semibold text-center mb-4 text-gray-800">Become a Driver</h3>
         <p className="text-gray-700">
-          Ready to earn with flexibility? Becoming a Cabjak driver lets you earn 100% of your trip fares, offering complete financial independence. Enjoy daily, weekly, and monthly rewards, including the chance to win exciting prizes. Get started by registering today!
+          Ready to earn with flexibility? Becoming a Cabjak driver let's you earn 100% of your trip fares, offering complete financial independence. Enjoy daily, weekly, and monthly rewards, including the chance to win exciting prizes. Get started by registering today!
         </p>
       </div>
       <Footer />
